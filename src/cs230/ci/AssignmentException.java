e39d308ec65de8807d13caec3b9025d12542629b
TSCHNEIDE002 <tschneide002@vplinacad23.ad.csbsju.edu>
2025-03-27 14:01:31 -0500

    Added doc

--- src/cs230/ci/AssignmentException.java
+++ src/cs230/ci/AssignmentException.java
@@ -4,6 +4,7 @@ public class AssignmentException extends Exception {
 
 	private static final long serialVersionUID = 1;
 
+	/** Sup */
 	public AssignmentException(String message) {
 		super(message);
 	}
